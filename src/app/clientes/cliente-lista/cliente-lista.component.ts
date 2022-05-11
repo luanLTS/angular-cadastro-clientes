06b05c61a32aba811eed138b4793c457159dcc54
Luan Teixeira <luanlts@outlook.com>
2022-05-11 16:24:27 -0300

    fix(core): uso da api rxjs para atualização da lista mediante adições de novos clientes
    utilizado o subject da api rxjs para reagir as modificações feitas na lista de clientes, enviando uma copia da lista para todos as classes que se inscreveram para receber as atualizações

--- src/app/clientes/cliente-lista/cliente-lista.component.ts
+++ src/app/clientes/cliente-lista/cliente-lista.component.ts
@@ -1,13 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { Cliente } from '../cliente.model';
 import { ClienteServices } from '../cliente.service';
-
 @Component({
   selector: 'app-cliente-lista',
   templateUrl: './cliente-lista.component.html',
   styleUrls: ['./cliente-lista.component.css'],
 })
-export class ClienteListaComponent implements OnInit {
+export class ClienteListaComponent implements OnInit, OnDestroy {
+  private clientesSubscription: Subscription;
   clientes: Cliente[] = [];
 
   // a lista de parametros do construtor são as dependencias desta classe
@@ -15,5 +16,14 @@ export class ClienteListaComponent implements OnInit {
 
   ngOnInit(): void {
     this.clientes = this.clienteService.getClientes();
+    this.clientesSubscription = this.clienteService
+      .getListaClientesAtualizadaObservable()
+      .subscribe((clientes: Cliente[]): void => {
+        this.clientes = clientes;
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.clientesSubscription.unsubscribe();
   }
 }
--- src/app/clientes/cliente.service.ts
+++ src/app/clientes/cliente.service.ts
@@ -1,8 +1,9 @@
 import { Cliente } from './cliente.model';
-
+import { Subject } from 'rxjs';
 // single source of truth
 export class ClienteServices {
   private clientes: Cliente[] = [];
+  private listaClientesAtualizada = new Subject<Cliente[]>(); // o evento vai gerar uma lista de clientes
 
   getClientes(): Cliente[] {
     return [...this.clientes]; // envia uma copia da lista, não a referenca dela
@@ -14,5 +15,10 @@ export class ClienteServices {
       fone,
       email,
     });
+    this.listaClientesAtualizada.next([...this.clientes]);
+  }
+
+  getListaClientesAtualizadaObservable() {
+    return this.listaClientesAtualizada.asObservable();
   }
 }
