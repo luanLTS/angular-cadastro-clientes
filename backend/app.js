1008566080b1191a25a40c55e2d7cf2e5a98fcf0
Luan Teixeira <luanlts@outlook.com>
2022-05-11 18:29:15 -0300

    apostila 03: fim da secao 2.2
    implementacao do endpoint para retornar a lista de clientes armazenados localmente

--- backend/app.js
+++ backend/app.js
@@ -1,17 +1,31 @@
 // este arquivo é um modulo js
 
 const express = require("express");
+const { rmdirSync } = require("fs");
 const app = express();
 
-// funcao middleware, fica no meio do caminho e pega as requisicoes
-app.use((req, res, next) => {
-  console.log("chegou uma requisicao");
-  next();
-});
+const clientes = [
+  {
+    id: '1',
+    nome: "Joao",
+    fone: '123456789',
+    email: 'joao@cliente.com'
+  },
+  {
+    id: '2',
+    nome: "maria",
+    fone: '123456789',
+    email: 'maria@cliente.com'
+  }
+]
 
-app.use((req, res) => {
-  res.send("Hello from the Back End now with Express");
-})
+// funcao middleware, fica no meio do caminho e pega as requisicoes
+app.use('/api/clientes', (req, res) => {
+  res.status(200).json({
+    message: 'OK',
+    clientes: clientes
+  });
+});
 
 // torna o objeto app public para ser utilizado em outras partes do projeto
 module.exports = app;
